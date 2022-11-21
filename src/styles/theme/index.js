ca4b02b107153f8f4d615ad3c0c98a66ab7b8fd2
Michaela-K <michaelakodes@gmail.com>
2022-11-21 08:52:57 -0500

    add theme WIP

---
+++ src/styles/theme/index.js
@@ -0,0 +1,33 @@
+import { createTheme } from "@mui/material";
+
+export const Colors = {
+  screen: '#C5C1C0',
+  steel: '#0A1612',
+  denim: '#1A2930',
+  marigold: '#F7CE3E',
+
+  primary: "#5f2c3e",
+  secondary: "#d1adcc",
+  success: "#4CAF50",
+  info: "#00a2ff",
+  danger: "#FF5722",
+  warning: "#FFC107",
+  dark: "#0e1b20",
+  light: "#aaa",
+  muted: "#abafb3",
+  border: "#DDDFE1",
+  inverse: "#2F3D4A",
+  shaft: "#333",
+  ///////////////
+  // Grays
+  ///////////////
+  dim_grey: "#696969",
+  dove_gray: "#d5d5d5",
+  body_bg: "#f3f6f9",
+  light_gray: "rgb(230,230,230)",
+  ///////////////
+  // Solid Color
+  ///////////////
+  white: "#fff",
+  black: "#000",
+};
