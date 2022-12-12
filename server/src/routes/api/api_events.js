360dd92a7758dcc1cdf8b1319bf946ea3bf62c92
Michaela-K <michaelakodes@gmail.com>
2022-12-12 13:06:23 -0500

    add search to api events

--- server/src/routes/api/api_events.js
+++ server/src/routes/api/api_events.js
@@ -2,12 +2,36 @@ const {Router} = require('express');
 const router = Router();
 
 module.exports = (pool) => {
-// GET api/events
-router.get("/", (req, res) => {
-  // res.send('List should be in the Terminal Console');
-  return pool.query(`SELECT * FROM events`)
+// GET api/events/search
+  router.get("/search", (req, res) => {
+    const id = req.params.id;
+    const query = req.params.query;
+    pool
+    .query(`
+        SELECT *
+        FROM events
+        WHERE title LIKE $1;`
+        , ['%'||query||'%'])
+        .then(data => {
+            // console.log("api_events data", data);
+        console.log("query Data", data);
+        const eventsData = data.rows;
+        console.log("api_events searchData",eventsData)
+        res.json(eventsData);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+// GET api/events/:id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  return pool.query(`SELECT * FROM events WHERE id = $1`, [id])
   .then(result => {
-    return res.json(result.rows)
+    return res.json(result.rows[0])
   })
   .catch(err => {
     res
@@ -16,12 +40,12 @@ router.get("/", (req, res) => {
   });
 });
 
-// GET api/events/:id
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  return pool.query(`SELECT * FROM events WHERE id = $1`, [id])
+// GET api/events
+router.get("/", (req, res) => {
+  // res.send('List should be in the Terminal Console');
+  return pool.query(`SELECT * FROM events`)
   .then(result => {
-    return res.json(result.rows[0])
+    return res.json(result.rows)
   })
   .catch(err => {
     res
