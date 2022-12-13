600da61583cdee16f733790d00192b2d9abce3aa
Michaela-K <michaelakodes@gmail.com>
2022-12-13 05:33:10 -0500

    rename routes

--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,7 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan('dev'))
+app.use(express.json())
 // const router = express.Router();
 // const cookieSession = require('cookie-session');
 // app.use(cookieSession({
@@ -23,10 +24,10 @@ console.log("server on port 4000")
 
 app.use(express.static("public"));
 
-const usersRouter = require('./routes/usersRoutes');
+const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
 
-const eventsRouter = require('./routes/eventsRoutes');
+const eventsRouter = require('./routes/events');
 app.use('/events', eventsRouter(pool));
 
 //API ROUTE HANDLERS
